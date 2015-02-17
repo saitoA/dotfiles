47357053ec260408110ae3cbd6c81d34b726e463
saitoA <postcardfromg@gmail.com>
2015-02-17 23:45:43 +0900

    added gitconfig

--- README.md
+++ README.md
@@ -1,7 +1,1 @@
 # dotfiles
-
-$ cd
-$ ln -s dotfiles/.vimrc .vimrc
-$ ln -s dotfiles/.bashrc .bashrc
-$ ln -s dotfiles/.vim .vim
-
--- setup.sh
+++ setup.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
-cd
+cd ${HOME}
 ln -s dotfiles/.vimrc .
 ln -s dotfiles/.bashrc .
 ln -s dotfiles/.vim .
+ln -s dotfiles/.gitconfig .
+ln -s dotfiles/.gitattributes .
 
+cat << _EOT_ > .gitconfig_local
+[user]
+  name = saitoA
+  email = postcardfromg@gmail.com
+_EOT_
